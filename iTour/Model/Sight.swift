503eaa59e43364a877e7c7674fde15353f258a7e
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2025-01-27 19:20:48 +0100

    working with relationships

---
+++ iTour/Model/Sight.swift
@@ -0,0 +1,16 @@
+//
+//  Sight.swift
+//  iTour
+//
+//  Created by Mohamed Alwakil on 2025-01-27.
+//
+import SwiftData
+
+@Model
+class Sight {
+    var name: String
+
+    init(name: String) {
+        self.name = name
+    }
+}
