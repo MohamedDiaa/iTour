052b7ecf5ee886487920f1441662920320cf59d9
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2025-01-27 17:12:29 +0100

    import swift data

---
+++ iTour/Destination.swift
@@ -0,0 +1,24 @@
+//
+//  Destination.swift
+//  iTour
+//
+//  Created by Mohamed Alwakil on 2025-01-27.
+//
+import SwiftUI
+import SwiftData
+
+@Model
+class Destination {
+
+    var name: String
+    var details: String
+    var date: Date
+    var priority: Int
+
+    init(name: String = "", details: String = "", date: Date = .now, priority: Int = 2) {
+        self.name = name
+        self.details = details
+        self.date = date
+        self.priority = priority
+    }
+}
