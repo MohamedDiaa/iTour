eb112893b5c3fb506305b62e8ef1a4b817195494
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2025-01-27 18:50:55 +0100

    sorting query results

--- iTour/ContentView.swift
+++ iTour/ContentView.swift
@@ -9,60 +9,51 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-
-    @Query var destinations: [Destination]
+    
     @State private var path = [Destination]()
-
+    
     @Environment(\.modelContext) var modelContext
+    @State private var sortOrder = SortDescriptor(\Destination.name)
 
     var body: some View {
-
+        
         NavigationStack(path: $path) {
-            List {
-                ForEach(destinations) { destination in
+            
+            DestinationListingView(sort: sortOrder)
+                .toolbar {
+                    Button("Add Destination", systemImage: "plus", action: addDestination)
 
-                    NavigationLink(value: destination) {
-                        VStack(alignment: .leading) {
-                            Text(destination.name)
-                                .font(.headline)
+                    Menu("Sort", systemImage: "arrow.up.arrow.down") {
+                        Picker("Sort", selection: $sortOrder) {
+                            Text("Name")
+                                .tag(SortDescriptor(\Destination.name))
 
-                            Text(destination.date.formatted(date: .long, time: .shortened))
+                            Text("Priority")
+                                .tag(SortDescriptor(\Destination.priority, order: .reverse))
+
+                            Text("Date")
+                                .tag(SortDescriptor(\Destination.date))
                         }
+                        .pickerStyle(.inline)
                     }
                 }
-                .onDelete(perform: deleteDestinations)
-
-            }
-            .toolbar {
-                Button("Add Destination", systemImage: "plus", action: addDestination)
-            }
-            .navigationTitle("iTour")
-            .navigationDestination(for: Destination.self, destination: EditDestinationView.init)
+                .navigationTitle("iTour")
+                .navigationDestination(for: Destination.self, destination: EditDestinationView.init)
             
         }
-
     }
-
-
+    
     func addDestination() {
         let destination = Destination()
         modelContext.insert(destination)
         path = [destination]
     }
-
-
-    func deleteDestinations(_ indexSet: IndexSet) {
-        for index in indexSet {
-            let destination = destinations[index]
-            modelContext.delete(destination)
-        }
-    }
-
+    
     func addSamples() {
         let rome = Destination(name: "Rome")
         let florence = Destination(name: "Florence")
         let naples = Destination(name: "Naples")
-
+        
         modelContext.insert(rome)
         modelContext.insert(florence)
         modelContext.insert(naples)
