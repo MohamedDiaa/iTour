7f065ccab0b2d9ffdabb2ee0aa537207884139df
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2025-01-27 17:40:10 +0100

    Querying swiftdata object in swiftui

--- iTour/ContentView.swift
+++ iTour/ContentView.swift
@@ -6,16 +6,41 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+
+    @Query var destinations: [Destination]
+    @Environment(\.modelContext) var modelContext
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+        NavigationStack {
+            List {
+                ForEach(destinations) { destination in
+                    VStack(alignment: .leading) {
+                        Text(destination.name)
+                            .font(.headline)
+
+                        Text(destination.date.formatted(date: .long, time: .shortened))
+                    }
+                }
+            }
+            .navigationTitle("iTour")
+            .toolbar {
+                Button("Add Samples", action: addSamples)
+            }
         }
-        .padding()
+    }
+
+    func addSamples() {
+        let rome = Destination(name: "Rome")
+        let florence = Destination(name: "Florence")
+        let naples = Destination(name: "Naples")
+
+        modelContext.insert(rome)
+        modelContext.insert(florence)
+        modelContext.insert(naples)
     }
 }
 
