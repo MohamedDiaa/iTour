f0e1791f8589f925d82252e34d63c536935fa70b
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2025-01-27 18:17:28 +0100

    Creating, editing, deleting model objects

--- iTour/ContentView.swift
+++ iTour/ContentView.swift
@@ -11,25 +11,50 @@ import SwiftData
 struct ContentView: View {
 
     @Query var destinations: [Destination]
+    @State private var path = [Destination]()
+
     @Environment(\.modelContext) var modelContext
 
     var body: some View {
 
-        NavigationStack {
+        NavigationStack(path: $path) {
             List {
                 ForEach(destinations) { destination in
-                    VStack(alignment: .leading) {
-                        Text(destination.name)
-                            .font(.headline)
 
-                        Text(destination.date.formatted(date: .long, time: .shortened))
+                    NavigationLink(value: destination) {
+                        VStack(alignment: .leading) {
+                            Text(destination.name)
+                                .font(.headline)
+
+                            Text(destination.date.formatted(date: .long, time: .shortened))
+                        }
                     }
                 }
+                .onDelete(perform: deleteDestinations)
+
+            }
+            .toolbar {
+                Button("Add Destination", systemImage: "plus", action: addDestination)
             }
             .navigationTitle("iTour")
-            .toolbar {
-                Button("Add Samples", action: addSamples)
-            }
+            .navigationDestination(for: Destination.self, destination: EditDestinationView.init)
+            
+        }
+
+    }
+
+
+    func addDestination() {
+        let destination = Destination()
+        modelContext.insert(destination)
+        path = [destination]
+    }
+
+
+    func deleteDestinations(_ indexSet: IndexSet) {
+        for index in indexSet {
+            let destination = destinations[index]
+            modelContext.delete(destination)
         }
     }
 
